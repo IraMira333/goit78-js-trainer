5416d77a6a6c97e291650b4115201a163d4c25f3
IraMira333 <irinat333@gmail.com>
2023-03-27 13:35:00 +0300

    Update java-module4-2.js

--- java-module4-2.js
+++ java-module4-2.js
@@ -515,71 +515,71 @@
 //Доповни функцію getUserWithEmail(users, email) таким чином, щоб вона повертала
 //об'єкт користувача, пошта якого (властивість email) збігається зі значенням
 //параметра email.
-const users = [
- {
-  name: 'Moore Hensley',
-  email: 'moorehensley@indexia.com',
-  eyeColor: 'blue',
-  friends: ['Sharron Pace'],
-  isActive: false,
-  balance: 2811,
-  gender: 'male'
- },
- {
-  name: 'Sharlene Bush',
-  email: 'sharlenebush@tubesys.com',
-  eyeColor: 'blue',
-  friends: ['Briana Decker', 'Sharron Pace'],
-  isActive: true,
-  balance: 3821,
-  gender: 'female'
- },
- {
-  name: 'Ross Vazquez',
-  email: 'rossvazquez@xinware.com',
-  eyeColor: 'green',
-  friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-  isActive: false,
-  balance: 3793,
-  gender: 'male'
- },
- {
-  name: 'Elma Head',
-  email: 'elmahead@omatom.com',
-  eyeColor: 'green',
-  friends: ['Goldie Gentry', 'Aisha Tran'],
-  isActive: true,
-  balance: 2278,
-  gender: 'female'
- },
- {
-  name: 'Carey Barr',
-  email: 'careybarr@nurali.com',
-  eyeColor: 'blue',
-  friends: ['Jordan Sampson', 'Eddie Strong'],
-  isActive: true,
-  balance: 3951,
-  gender: 'male'
- },
- {
-  name: 'Blackburn Dotson',
-  email: 'blackburndotson@furnigeer.com',
-  eyeColor: 'brown',
-  friends: ['Jacklyn Lucas', 'Linda Chapman'],
-  isActive: false,
-  balance: 1498,
-  gender: 'male'
- },
- {
-  name: 'Sheree Anthony',
-  email: 'shereeanthony@kog.com',
-  eyeColor: 'brown',
-  friends: ['Goldie Gentry', 'Briana Decker'],
-  isActive: true,
-  balance: 2764,
-  gender: 'female'
- }
-];
+// const users = [
+//  {
+//   name: 'Moore Hensley',
+//   email: 'moorehensley@indexia.com',
+//   eyeColor: 'blue',
+//   friends: ['Sharron Pace'],
+//   isActive: false,
+//   balance: 2811,
+//   gender: 'male'
+//  },
+//  {
+//   name: 'Sharlene Bush',
+//   email: 'sharlenebush@tubesys.com',
+//   eyeColor: 'blue',
+//   friends: ['Briana Decker', 'Sharron Pace'],
+//   isActive: true,
+//   balance: 3821,
+//   gender: 'female'
+//  },
+//  {
+//   name: 'Ross Vazquez',
+//   email: 'rossvazquez@xinware.com',
+//   eyeColor: 'green',
+//   friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//   isActive: false,
+//   balance: 3793,
+//   gender: 'male'
+//  },
+//  {
+//   name: 'Elma Head',
+//   email: 'elmahead@omatom.com',
+//   eyeColor: 'green',
+//   friends: ['Goldie Gentry', 'Aisha Tran'],
+//   isActive: true,
+//   balance: 2278,
+//   gender: 'female'
+//  },
+//  {
+//   name: 'Carey Barr',
+//   email: 'careybarr@nurali.com',
+//   eyeColor: 'blue',
+//   friends: ['Jordan Sampson', 'Eddie Strong'],
+//   isActive: true,
+//   balance: 3951,
+//   gender: 'male'
+//  },
+//  {
+//   name: 'Blackburn Dotson',
+//   email: 'blackburndotson@furnigeer.com',
+//   eyeColor: 'brown',
+//   friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//   isActive: false,
+//   balance: 1498,
+//   gender: 'male'
+//  },
+//  {
+//   name: 'Sheree Anthony',
+//   email: 'shereeanthony@kog.com',
+//   eyeColor: 'brown',
+//   friends: ['Goldie Gentry', 'Briana Decker'],
+//   isActive: true,
+//   balance: 2764,
+//   gender: 'female'
+//  }
+// ];
 // const getUserWithEmail = (users, email) => {
 //  const userEmail = users.find((user) => user.email === email);
 //  console.log(userEmail);
@@ -729,10 +729,10 @@ const users = [
 //   return totalFriends;
 //  }, 0);
 
- const getTotalFriendCount = (users) =>
- users.reduce((total, { friends }) => total + friends.length, 0);
+//  const getTotalFriendCount = (users) =>
+//  users.reduce((total, { friends }) => total + friends.length, 0);
 
-console.log(getTotalFriendCount(users));
+// console.log(getTotalFriendCount(users));
 
 //!=============================================
 //Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву
@@ -938,8 +938,8 @@ console.log(getTotalFriendCount(users));
 //Доповни функцію getTotalBalanceByGender(users, gender) таким чином, щоб вона
 //повертала загальний баланс користувачів (властивість balance),
 //стать яких (властивість gender) збігається зі значенням параметра gender.
-const getTotalBalanceByGender = (users, gender) =>
- users
-  .filter((user) => user.gender === gender)
-  .reduce((total, { balance }) => total + balance, 0);
-console.table(getTotalBalanceByGender(users, 'male'));
+// const getTotalBalanceByGender = (users, gender) =>
+//  users
+//   .filter((user) => user.gender === gender)
+//   .reduce((total, { balance }) => total + balance, 0);
+// console.table(getTotalBalanceByGender(users, 'male'));
